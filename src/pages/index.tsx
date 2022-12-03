9a171523b51f757065259d0a5239688e47a162cb
Bit-Barron <1bit.baron@gmail.com>
2022-12-03 16:00:28 +0100

    hello

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import Header from './components/Header';
 import Navbar from './components/Navbar';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CryptoData } from '../types';
-import {AiFillFire} from 'react-icons/ai'
-import {AiOutlineClockCircle} from 'react-icons/ai'
-import {AiFillStar} from 'react-icons/ai'
 
 export default function Home() {
   const [coins, setCoins] = useState<CryptoData>();
@@ -21,29 +17,34 @@ export default function Home() {
 
   return (
     <form>
-      <Header />
-      <div className='bg-[#191a1e]'>
-        <Navbar />
-      </div>
+      <Navbar />
       <div>
-        <h1 className='font-bold text-2xl text-center mt-5'>
+        <h1 className='font-bold text-2xl text-center mt-5 '>
           Today's Cryptocurrency Prices by Market track
         </h1>
       </div>
-
-      <div className='flex gap-3 rounded-lg  justify-center'>
-        <div className='w-72 h-20 mx-automt mt-10 bg-[#181a1b] '>
-          <div className='mt-2 ml-4 flex'><span className='mt-1 text-red-500'><AiFillFire /></span>Trending</div>
+      <div></div>
+      <div className='flex justify-center gap-2'>
+        <div className='w-[400px] h-20 mx-automt mt-10 bg-[#181a1b] '>
+          <div className='mt-2 ml-4 flex'>
+            <span className='mt-1 text-red-500'></span>
+            Trending
+          </div>
         </div>
-        <div className='w-72 h-20 mx-automt mt-10 bg-[#181a1b] '>
-        <div className='mt-2 ml-4 flex'><span className='mt-1 text-blue-500'><AiOutlineClockCircle /></span>Recently added</div>
+        <div className='w-[400px]  h-20 mx-automt mt-10 bg-[#181a1b] '>
+          <div className='mt-2 ml-4 flex'>
+            <span className='mt-1 text-blue-500'></span>
+            Recently added
+          </div>
         </div>
-        <div className='w-72 h-20 mx-automt mt-10 bg-[#181a1b] '>
-        <div className='mt-2 ml-4 flex'><span className='mt-1 text-yellow-500'><AiFillStar /></span>Top Community artivle</div>
+        <div className='w-[400px]  h-20 mx-automt mt-10 bg-[#181a1b] '>
+          <div className='mt-2 ml-4 flex'>
+            <span className='mt-1 '>Top Community artivle</span>
+          </div>
         </div>
       </div>
 
-      <div className='overflow-x-auto relative container mt-96 mx-auto '>
+      <div className='overflow-x-auto relative container mt-10 mx-auto '>
         <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
           <thead className='text-xs text-white text-bold uppercase  '>
             <tr>
@@ -52,6 +53,9 @@ export default function Home() {
               </th>
               <th scope='col' className='py-3 px-6'>
                 Price
+              </th>
+              <th scope='col' className='py-3 px-6'>
+                Symbol
               </th>
               <th scope='col' className='py-3 px-6'>
                 1h %
@@ -79,23 +83,25 @@ export default function Home() {
               <tbody>
                 <tr className='border-b dark:border-gray-700'>
                   <th
-                    scope='row'
-                    className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                    scope=''
+                    className='py-4 px-6 font-medium text-gray-900  dark:text-white'
                   >
                     {coin.name}
                   </th>
                   <td className='py-4 px-6'>{coin.quote.USD.price}</td>
+                  <td className='py-4 px-6'>{coin.symbol}</td>
                   <td className='py-4 px-6'>
                     {coin.quote.USD.percent_change_1h}
                   </td>
+
                   <td className='py-4 px-6'>
                     {coin.quote.USD.percent_change_24h}
                   </td>
                   <td className='py-4 px-6'>
                     {coin.quote.USD.percent_change_7d}
                   </td>
-                  <td className='py-4 px-6'>{coin.quote.USD.market_cap}</td>
-                  <td className='py-4 px-6'>{coin.quote.USD.volume_24h}</td>
+                  <td className='py-4 px-6'>${coin.quote.USD.market_cap}</td>
+                  <td className='py-4 px-6'>${coin.quote.USD.volume_24h}</td>
                 </tr>
               </tbody>
             </>
