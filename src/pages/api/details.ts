0115965e3e01c1b9809c1feb5815e999d49d9c0b
Bit-Barron <1bit.baron@gmail.com>
2022-12-04 00:35:46 +0100

    details.ts

--- src/pages/api/details.ts
+++ src/pages/api/details.ts
@@ -10,16 +10,16 @@ export default async function handler(
   if (req.method === "GET") {
     const query = req.query;
     const { id, name } = query;
-    const result = await axios.post(
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fe979189-cae7-490e-8cbd-66158a83141d",
-      {
-        headers: {
-          "X-CMC_PRO_API_KEY": "fe979189-cae7-490e-8cbd-66158a83141d",
-          "Content-Type": "application/json",
-          "Accept-Encoding": "application/json",
-        },
-      }
-    );
+    // const result = await axios.post(
+    //   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fe979189-cae7-490e-8cbd-66158a83141d",
+    //   {
+    //     headers: {
+    //       "X-CMC_PRO_API_KEY": "fe979189-cae7-490e-8cbd-66158a83141d",
+    //       "Content-Type": "application/json",
+    //       "Accept-Encoding": "application/json",
+    //     },
+    //   }
+    // );
   }
 
   res.status(200).json({ message: "test" });
