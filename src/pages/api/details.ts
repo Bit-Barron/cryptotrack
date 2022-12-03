3339a0a7485a9532c1cb46a3740b5f372bde2a3e
Bit-Barron <1bit.baron@gmail.com>
2022-12-04 00:30:00 +0100

    details

--- src/pages/api/details.ts
+++ src/pages/api/details.ts
@@ -1,16 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CryptoList } from "../../types";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     const query = req.query;
     const { id, name } = query;
-    console.log(query.id);
-    console.log(query.name);
+    const result = await axios.post(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fe979189-cae7-490e-8cbd-66158a83141d",
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": "fe979189-cae7-490e-8cbd-66158a83141d",
+          "Content-Type": "application/json",
+          "Accept-Encoding": "application/json",
+        },
+      }
+    );
   }
 
-  res.status(200).json({ message: 'test' });
+  res.status(200).json({ message: "test" });
 }
