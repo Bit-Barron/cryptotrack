2d2b3dd7a83b8981bd774cf4cba730c54e0d50b3
Bit-Barron <1bit.baron@gmail.com>
2022-12-04 14:35:54 +0100

    id.tsx

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import Informations from "../../components/elements/Detail/Informations";
 import Navbar from "../../components/Navbar/Navbar";
 import { CryptoDetails } from "../../types";
 
@@ -23,56 +22,41 @@ const Details = () => {
     <>
       <Navbar />
       <div className="flex">
-        <div className="ml-10 mt-5 w-1/2 rounded-lg bg-[#25262d] p-5">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="ml-10  mr-10 mt-5 w-full rounded-lg bg-[#25262d] p-5">
+          <div className="relative w-full overflow-x-auto shadow-md sm:rounded-lg">
             <div className="mb-2 text-2xl font-bold text-white">Details</div>
             <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-              <thead className="text-xs uppercase text-gray-700 dark:text-gray-400">
-                <tr>
+              <tbody>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
                   <th
-                    scope="col"
-                    className="py-3 px-6 "
+                    scope="row"
+                    className="whitespace-nowrappy-4 px-6 font-medium text-white"
                   >
-                    Product name
+                    Name
                   </th>
+
+                  <td className="py-4 px-6">{isData?.data[`${id}`].name}</td>
+                </tr>
+
+                <tr className="border-b border-gray-200 dark:border-gray-700">
                   <th
-                    scope="col"
-                    className=" py-3 px-6"
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
                   >
                     Symbol
                   </th>
-                  <th
-                    scope="col"
-                    className="py-3 px-6"
-                  >
-                    24h
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3 px-6 "
-                  >
-                    7d
-                  </th>
+                  <td className="py-4 px-6">{isData?.data[`${id}`].symbol}</td>
                 </tr>
-              </thead>
-              <tbody>
-                <tr className="border-b border-gray-700 text-white">
-                  <th
-                    scope="row"
-                    className="whitespace-nowrap  py-4 px-6 font-medium text-gray-900 "
-                  >
-                    <span className="text-white">
-                      {isData?.data[`${id}`].name}
-                    </span>
-                  </th>
-                  <td className="py-4 px-6"> {isData?.data[`${id}`].symbol}</td>
-                  <td className=" py-4 px-6 ">
-                    {isData?.data[`${id}`].quote.USD.percent_change_24h}
-                  </td>
-                  <td className=" py-4 px-6 ">
-                    {isData?.data[`${id}`].quote.USD.percent_change_7d}
-                  </td>
-                </tr>
+                <th
+                  scope="row"
+                  className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                >
+                  Max Supplay
+                </th>
+                <td className="py-4 px-6">
+                  {isData?.data[`${id}`].max_supply}
+                </td>
+                <tr></tr>
               </tbody>
             </table>
           </div>
@@ -94,33 +78,97 @@ const Details = () => {
                     ${isData?.data[`${id}`].quote.USD.price.toFixed(2)}
                   </td>
                 </tr>
-
                 <tr className="border-b border-gray-200 dark:border-gray-700">
                   <th
                     scope="row"
                     className="whitespace-nowrap py-4 px-6 font-medium text-white"
                   >
-                    24 Volumen
+                    Last Udpdated
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.last_updated}EUR
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    Market Cap
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.market_cap}EUR
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    1h
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.percent_change_1h}EUR
+                  </td>
+                </tr>{" "}
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    24h 
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.percent_change_24h}EUR
+                  </td>
+                </tr>{" "}
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    30d
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.percent_change_30d}EUR
+                  </td>
+                </tr>{" "}
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    24h Volumen
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.volume_24h}EUR
+                  </td>
+                </tr>{" "}
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    24h Volumen
+                  </th>
+                  <td className="py-4 px-6">
+                    {isData?.data[`${id}`].quote.USD.volume_24h}EUR
+                  </td>
+                </tr>{" "}
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap py-4 px-6 font-medium text-white"
+                  >
+                    24h Volumen
                   </th>
                   <td className="py-4 px-6">
                     {isData?.data[`${id}`].quote.USD.volume_24h}EUR
                   </td>
                 </tr>
-                <th
-                  scope="row"
-                  className="whitespace-nowrap py-4 px-6 font-medium text-white"
-                >
-                  Market Cap
-                </th>
-                <td className="py-4 px-6">
-                  {isData?.data[`${id}`].quote.USD.last_updated}
-                </td>
               </tbody>
             </table>
           </div>
-        </div>
-        <div className="ml-52">
-          <Informations />
         </div>
       </div>
     </>
