649b62f26d7a860239258ff5ddc1cd3b95d7d736
Bit-Barron <1bit.baron@gmail.com>
2022-12-04 12:23:44 +0100

    id

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Informations from "../../components/elements/Detail/Informations";
-import Table from "../../components/elements/Detail/Table";
 import Navbar from "../../components/Navbar/Navbar";
 import { CryptoDetails } from "../../types";
 
@@ -23,10 +22,24 @@ const Details = () => {
   return (
     <>
       <Navbar />
-      <div className="flex justify-start mb-10 mt-10">
-        <div className="bg-[#25262d] p-10">
-          <div className="text-2xl">
-            Bitcoin 
+      <div className="mb-10 mt-10 flex justify-start">
+        <div className="relative mb-8 flex w-[900px] min-w-0 flex-col break-words rounded-lg bg-[#25262d] shadow-lg">
+          <div className="mb-0 rounded-t bg-transparent px-4 py-3">
+            <div className="flex flex-wrap items-center">
+              <div className="relative w-full max-w-full flex-1 flex-grow">
+                <h6 className="text-blueGray-500 mb-1 text-xs font-semibold uppercase">
+                  Performance
+                </h6>
+                <h2 className="text-blueGray-800 text-xl font-semibold text-white">
+                  Total orders
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="flex-auto p-4">
+            <div className="h-350-px relative">
+              <canvas width="221" height="291" id="bar-chart"></canvas>
+            </div>
           </div>
         </div>
       </div>
