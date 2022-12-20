5e44a88b135d0edc187121c51558407a9ae2ec72
Bit-Barron <1bit.baron@gmail.com>
2022-12-20 21:57:33 +0100

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 import axios from "axios";
 import Image from "next/image";
 import router from "next/router";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Navbar from "../components/NavbarContainer";
 import { useStores } from "../stores";
 import { CryptoCurrencyApiResponse } from "../types";
@@ -96,6 +97,74 @@ export default function Home() {
             search
           </button>
         </div>
+        <Menu as="div" className="relative ml-40 inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+              Options
+              <ChevronDownIcon
+                className="-mr-1 ml-2 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              onClick={price}
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  <a href="#" className="" onClick={() => setClick("usd")}>
+                    usd
+                  </a>
+                </Menu.Item>
+                <div>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        type="submit"
+                        onClick={() => setClick("btc")}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block w-full px-4 py-2 text-left text-sm"
+                        )}
+                      >
+                        btc
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        type="submit"
+                        onClick={() => setClick("eur")}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block w-full px-4 py-2 text-left text-sm"
+                        )}
+                      >
+                        eur
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
 
         <div className="flex justify-end">
           <Transition
